65e6009fc5df3e1f15838bcab4b45629e10220ef
Ivan Vakov <119103300+IvanVakov@users.noreply.github.com>
2023-06-04 23:06:13 +0300

    Update guess_a_number.py

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,35 +1,53 @@
 import random
 
-computer_number = random.randint(1, 10)
-count = 0
-tries_count = 5
 
-while True:
-    player_input = input("Guess the number (1-10): ")
-    print(f"You will have {tries_count} tries to guess it")
-    
-    count += 1
-    tries_count -= 1
+def play_game():
+    computer_number = random.randint(1, 10)
+    count = 0
+    tries_count = 5
 
-    if not player_input.isdigit():
-        print("Invalid input. Try again...")
-        continue
+    while True:
+        player_input = input("Guess the number (1-10): ")
+        print(f"You will have {tries_count} tries to guess it")
 
-    player_number = int(player_input)
+        count += 1
+        tries_count -= 1
 
-    if player_number == computer_number:
-        print("You guess it!")
-        break
-    elif player_number > computer_number:
-        print("Too High!")
-    else:
-        print("Too Low!")
-        
-    if count > 5:
-        if player_number != computer_number:
-            print("Sorry you run out of tries. Try again in the next game.")
+        if not player_input.isdigit():
+            print("Invalid input. Try again...")
+            continue
+
+        player_number = int(player_input)
+
+        if player_number == computer_number:
+            print("You guessed it!")
+            break
+        elif player_number > computer_number:
+            print("Too High!")
+        else:
+            print("Too Low!")
+
+        if count > 5:
+            print("Sorry, you ran out of tries.")
+            break
+
+
+def play_again():
+    while True:
+        restart = input("Do you want to play again? (yes/no): ")
+        if restart.lower() == "yes":
+            play_game()
+        elif restart.lower() == "no":
+            print("Thank you for playing! Goodbye!")
             break
         else:
-             print("You guess it!")
-             break
-            
+            print("Invalid input. Please enter 'yes' or 'no'.")
+
+
+def start():
+    print("Welcome to the Number Guessing Game!")
+    play_game()
+    play_again()
+
+
+start()
